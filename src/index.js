1b32dd59d78c6d195224a6f8b9763bcfcfc2d24f
Routemate <info@routematellc.com>
2026-06-30 11:51:04 -0400

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-iimport App from './RouteMate';
+import App from './RouteMate';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
